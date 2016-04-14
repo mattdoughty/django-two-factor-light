1b8340d816f599ff8893b8d5033b44f379c0884f
Matt Doughty <mattdoughty6521@gmail.com>
2016-04-13 20:36:09 -0700

    Add setup_url property

--- two_factor_light/models.py
+++ two_factor_light/models.py
@@ -5,11 +5,13 @@ from os import urandom
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from urllib import quote, urlencode
 
 from .oath import TOTP
 
 
 TWO_FACTOR_TOLERANCE = getattr(settings, 'TWO_FACTOR_TOLERANCE', 1)
+TWO_FACTOR_ISSUER = getattr(settings, 'TWO_FACTOR_ISSUER', 'Django App')
 
 
 class AbstractTwoFactorUser(models.Model):
@@ -40,6 +42,20 @@ class AbstractTwoFactorUser(models.Model):
                 return True
 
         return False
+
+    @property
+    def setup_url(self):
+        issuer = TWO_FACTOR_ISSUER.encode('utf-8')
+
+        label = quote(b': '.join([issuer, self.two_factor_custom_name]))
+
+        query = [
+            ('secret', self.two_factor_secret),
+            ('issuer', issuer),
+        ]
+
+        return 'otpauth://totp/{}?{}'.format(label, urlencode(query))
+
 
 
 class TwoFactorUser(AbstractTwoFactorUser, AbstractUser):
