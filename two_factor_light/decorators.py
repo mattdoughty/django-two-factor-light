a8b6fe0e6afc28c72f0071c769c3b2498b2451d7
Matt Doughty <mattdoughty6521@gmail.com>
2016-04-13 20:46:00 -0700

    Don't use wrapping for decorator

--- two_factor_light/decorators.py
+++ two_factor_light/decorators.py
@@ -1,9 +1,6 @@
-from functools import wraps
-
 from django.conf import settings
 from django.contrib.auth import BACKEND_SESSION_KEY, REDIRECT_FIELD_NAME
 from django.shortcuts import resolve_url
-from django.utils.decorators import available_attrs
 from django.utils.six.moves.urllib.parse import urlparse
 
 
@@ -11,7 +8,7 @@ TWO_FACTOR_LOGIN_URL = getattr(settings, 'TWO_FACTOR_LOGIN_URL', settings.LOGIN_
 TWO_FACTOR_SETUP_URL = getattr(settings, 'TWO_FACTOR_SETUP_URL', None)
 
 
-def full_login_required(redirect_field_name=REDIRECT_FIELD_NAME, login_url=None, two_factor_login_url=None,
+def full_login_required(view_func, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None, two_factor_login_url=None,
                         two_factor_setup_url=None):
     """
     Decorator for views that checks that the user is logged in with 2FA, if applicable.
@@ -20,35 +17,31 @@ def full_login_required(redirect_field_name=REDIRECT_FIELD_NAME, login_url=None,
     Redirects to TWO_FACTOR_SETUP_URL if logged in but hasn't yet setup 2FA.
     Redirects to LOGIN_URL if not logged in at all.
     """
-    def decorator(view_func):
-        @wraps(view_func, assigned=available_attrs(view_func))
-        def _wrapped_view(request, *args, **kwargs):
-            if (request.user.is_authenticated() and
-                    (request.session[BACKEND_SESSION_KEY] == "two_factor_light.backends.TwoFactorBackend" or
-                        not request.user.two_factor_required
-                     )):
-                return view_func(request, *args, **kwargs)
+    def decorator(request, *args, **kwargs):
+        if (request.user.is_authenticated() and
+                (request.session[BACKEND_SESSION_KEY] == "two_factor_light.backends.TwoFactorBackend" or
+                    not request.user.two_factor_required
+                 )):
+            return view_func(request, *args, **kwargs)
 
-            if not request.user.is_authenticated():
-                # If user isn't authenticated, do the same as login_required - redirect to login
-                resolved_redirect_url = resolve_url(login_url or settings.LOGIN_URL)
-            elif request.user.two_factor_enabled:
-                # If the user has two factor auth, redirect to two factor login url
-                resolved_redirect_url = resolve_url(two_factor_login_url or TWO_FACTOR_LOGIN_URL)
-            else:
-                # The user hasn't setup two factor auth yet, so redirect to setup url
-                resolved_redirect_url = resolve_url(two_factor_setup_url or TWO_FACTOR_SETUP_URL)
+        if not request.user.is_authenticated():
+            # If user isn't authenticated, do the same as login_required - redirect to login
+            resolved_redirect_url = resolve_url(login_url or settings.LOGIN_URL)
+        elif request.user.two_factor_enabled:
+            # If the user has two factor auth, redirect to two factor login url
+            resolved_redirect_url = resolve_url(two_factor_login_url or TWO_FACTOR_LOGIN_URL)
+        else:
+            # The user hasn't setup two factor auth yet, so redirect to setup url
+            resolved_redirect_url = resolve_url(two_factor_setup_url or TWO_FACTOR_SETUP_URL)
 
-            path = request.build_absolute_uri()
-            redirect_scheme, redirect_netloc = urlparse(resolved_redirect_url)[:2]
-            current_scheme, current_netloc = urlparse(path)[:2]
-            if ((not redirect_scheme or redirect_scheme == current_scheme) and
-                    (not redirect_netloc or redirect_netloc == current_netloc)):
-                path = request.get_full_path()
-            from django.contrib.auth.views import redirect_to_login
-            return redirect_to_login(
-                path, resolved_redirect_url, redirect_field_name
-            )
-
-        return _wrapped_view
+        path = request.build_absolute_uri()
+        redirect_scheme, redirect_netloc = urlparse(resolved_redirect_url)[:2]
+        current_scheme, current_netloc = urlparse(path)[:2]
+        if ((not redirect_scheme or redirect_scheme == current_scheme) and
+                (not redirect_netloc or redirect_netloc == current_netloc)):
+            path = request.get_full_path()
+        from django.contrib.auth.views import redirect_to_login
+        return redirect_to_login(
+            path, resolved_redirect_url, redirect_field_name
+        )
     return decorator
